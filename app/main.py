273ace355057ab1a3de780981b7d240f58b21037
likai97 <likai.becker@gmail.com>
2026-02-21 15:53:48 +0100

    annotated image first try

--- app/main.py
+++ app/main.py
@@ -1,27 +1,57 @@
+import os
+import uuid
+
 from fastapi import FastAPI, UploadFile, File, Request
 from fastapi.responses import HTMLResponse
-import pytesseract
-from PIL import Image
-
-# import uvicorn
-from app.utils import is_chinese, return_pinyin
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
 
+import pytesseract
+from PIL import Image, ImageOps
 
+# import uvicorn
+from app.utils import is_chinese, return_pinyin, generate_annotated_image
 
 app = FastAPI()
+font_path = "/usr/share/fonts/truetype/liberation/LiberationSans-Regular.ttf"
+UPLOAD_DIR = "app/static/uploads"
+os.makedirs(UPLOAD_DIR, exist_ok=True)
+
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
 templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/", response_class=HTMLResponse)
-def home(request: Request):
+async def home(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/upload", response_class=HTMLResponse)
-def upload_file(request: Request, file: UploadFile = File(...)):
+async def upload_file(request: Request, file: UploadFile = File(...)):
     image = Image.open(file.file)
+    
+    # Option 1
     chinese_texts = pytesseract.image_to_string(image, lang='chi_sim')
     html_output = return_pinyin(chinese_texts)
+
+    # Option 2
+    image_greyscale = image.convert('L') 
+    image_greyscale = ImageOps.autocontrast(image.convert('L')) # increase contrast
+    custom_config = r'--oem 3 --psm 3'
+    ocr_data = pytesseract.image_to_data(
+        image_greyscale,
+        lang='chi_sim', 
+        config=custom_config,
+        output_type=pytesseract.Output.DICT
+    )
+    annotated_img = generate_annotated_image(image.copy(), ocr_data, font_path)
+
+    unique_name = f"{uuid.uuid4()}.jpg"
+    file_path = os.path.join(UPLOAD_DIR, unique_name)
+    annotated_img.save(file_path)
+
     # edit the html output to include a link to go back to the home page
-    return templates.TemplateResponse("output.html", {"request": request, "html_output": html_output})
+    return templates.TemplateResponse("output.html", {
+        "request": request,
+        "html_output": html_output,
+        "annotated_url": f"/static/uploads/{unique_name}"
+    })
 
--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,6 @@
 from pypinyin import pinyin, Style
+from PIL import ImageDraw, ImageFont
+import pytesseract
 
 def is_chinese(char):
     return '\u4e00' <= char <= '\u9fff'
@@ -12,3 +14,48 @@ def return_pinyin(chinese_texts):
         else:
             ruby_elements.append(char)
     return ''.join(ruby_elements)
+
+def generate_annotated_image(image, data, font_path):
+    draw = ImageDraw.Draw(image)
+    for i, txt in enumerate(data['text']):
+        clean_txt = txt.strip()
+        
+        if not clean_txt:
+            continue
+        
+        x, y, w, h = data['left'][i], data['top'][i], data['width'][i], data['height'][i]
+        pinyin_font_size = max(10, int(h * 0.35)) # floor at 10  
+        current_font = ImageFont.truetype(font_path, pinyin_font_size)
+
+        chars_in_block = [c for c in clean_txt if is_chinese(c)]
+        
+        if chars_in_block:
+            char_width = w / len(clean_txt)
+            # get list of pinyins
+            py_list = pinyin(clean_txt, style=Style.TONE)
+
+            for char_idx, char in enumerate(clean_txt):
+                
+                if py_list:
+                    py = py_list[char_idx][0]
+
+                    # Temp bounding box for centering logic
+                    char_center_x = x + (char_idx * char_width) + (char_width / 2)
+                    temp_bbox = draw.textbbox((0, 0), py, font=current_font)
+                    py_w = temp_bbox[2] - temp_bbox[0]
+
+                    x_py_centered = char_center_x - (py_w / 2)
+                    y_py = y - pinyin_font_size
+
+                    # bounding boxes  
+                    bbox = draw.textbbox((x_py_centered, y_py), py, font=current_font)
+
+                    # padding
+                    pad = max(1, int(h * 0.05))
+                    padded_box = (bbox[0] - pad, bbox[1] - pad, bbox[2] + pad,bbox[3] + pad)
+
+                    # draw rectangle
+                    draw.rectangle(padded_box, fill="white")
+                    # draw text
+                    draw.text((x_py_centered, y_py), py, fill="red", font=current_font)
+    return image
